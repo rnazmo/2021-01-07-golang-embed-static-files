4ebe79892938e1b52bbfc000d66fb63405370dc2
rnazmo <rnazmo@gmail.com>
2021-01-07 01:54:27 +0900

    Update test: add support for config file

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"os"
 
+	"github.com/BurntSushi/toml"
 	"github.com/rakyll/statik/fs"
 	_ "github.com/rnazmo/sandbox__go_statik/statik"
 )
+
+type Config struct {
+	Foo Foo `toml:"foo"`
+	Bar Bar `toml:"bar"`
+}
+
+type Foo struct {
+	Words string `toml:"words"`
+}
+type Bar struct {
+	Number int `toml:"number"`
+}
 
 func main() {
 	statikFS, err := fs.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := statikFS.Open("/hello.sh")
+	f, err := statikFS.Open("/default.toml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	io.Copy(os.Stdout, f)
+	conf := new(Config)
+	_, err = toml.DecodeReader(f, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(conf)
+	fmt.Println(conf.Foo)
+	fmt.Println(conf.Foo.Words)
 }
